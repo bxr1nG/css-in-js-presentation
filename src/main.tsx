56cc7e0eb996c19423db720bece592b0341b15da
Daniil Voshchuk <dvoshchuk@objectstyle.com>
2024-10-01 08:47:52 +0300

    add deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>home</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
